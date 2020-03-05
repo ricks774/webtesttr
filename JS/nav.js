40a9ea1719bcc0074462214c57e6cb6b266a5df4
ricks <ricks774@gmail.com>
2020-03-05 18:43:24 +0800

    0305

---
+++ JS/nav.js
@@ -0,0 +1,10 @@
+const navSide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-list');
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+  });
+}
+
+navSide();
